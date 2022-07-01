84042c9f09f7b1d3f3b10dcd7c5b3d22a3f41cf5
Rafael Tiburcio <contatorafaeltiburcio@gmail.com>
2022-07-01 10:06:07 -0300

    criação da resultadoActivity

--- app/src/main/java/com/oceanbrasil/ocean_android_introducao_30_06_2022/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_introducao_30_06_2022/MainActivity.kt
@@ -3,6 +3,7 @@ package com.oceanbrasil.ocean_android_introducao_30_06_2022
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -13,13 +14,20 @@ class MainActivity : AppCompatActivity() {
         // Buscar o botão pelo ID
         val btEnviar = findViewById<Button>(R.id.btEnviar)
         val tvResultado = findViewById<TextView>(R.id.tvResultado)
+        val etNome = findViewById<EditText>(R.id.etNome )
+
 
         btEnviar.setOnClickListener {
             //Dentro desse bloco estamos dentro do clicklistener. Portanto o código
             //que está aqui será executado quando o botão for clicado.
 
-            tvResultado.text = "Rafael Tibbss"
+            val nomeDigitado = etNome.text
 
+            if (nomeDigitado.isBlank()) {
+                etNome.error = "Digite um nome Válido"
+            } else {
+                tvResultado.text = nomeDigitado
+            }
         }
     }
 }
